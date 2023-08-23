46c2d16cf291e6cb4d6604f37034f2754d19d48b
LeeDonSuk <leehyen132@gmail.com>
2023-08-23 17:09:37 +0900

    2023-08-23

--- js/index.js
+++ js/index.js
@@ -2,45 +2,59 @@
 //배너슬라이드
 //배너슬라이드
 function bannerslide() {
-        const slidenextbtn = document.querySelector(".slide-next");
-        const slidecontainer = document.querySelector(".slide-wrap");
-        const progressbar = document.querySelector(".silide-progressinner");
-        const slidenumberdata = document.querySelector(".number-data");
-
-
-        let currentSlide = 0;
-
-        slidenextbtn.addEventListener("click", function () {
-                slidecontainer.style.transform = `translateX(-${currentSlide * 100}%)`;
-                progressbar.style.width = `${(currentSlide + 1) * 25}%`; // Assuming 4 slides
-
-                currentSlide = (currentSlide + 1) % 4; // Assuming 4 slides
-                console.log(currentSlide)
-                if (currentSlide === 1) {
-                        slidenumberdata.innerHTML = `01 / 04`;
-                } else if (currentSlide === 2) {
-                        slidenumberdata.innerHTML = `02 / 04`;
-                } else if (currentSlide === 3) {
-                        slidenumberdata.innerHTML = `03 / 04`;
-                } else if (currentSlide === 0) {
-                        slidenumberdata.innerHTML = `04 / 04`;
-                }
-        });
-
+        var swiper = new Swiper(".banner-slide", {
+                pagination: {
+                  el: ".swiper-pagination",
+                  type: "progressbar",
+                },
+                navigation: {
+                  nextEl: ".swiper-button-next",
+                  prevEl: ".swiper-button-prev",
+                },
+              });
 }
 //인물소개 슬라이드
 //인물소개 슬라이드
 //인물소개 슬라이드
-var swiper = new Swiper(".mySwiper", {
-        navigation: {
-          nextEl: ".trnasform-next",
-          prevEl: ".trnasform-prev",
-        },
-      });
+function profileslide(){
+        var swiper = new Swiper(".mySwiper", {
+                navigation: {
+                  nextEl: ".trnasform-next",
+                  prevEl: ".trnasform-prev",
+                },
+              });
+}
+//스크롤 감지 이벤트
+//스크롤 감지 이벤트
+//스크롤 감지 이벤트
+
+function opserver(){
+  // 대상 요소들을 모두 선택합니다.
+const observedElements = document.querySelectorAll('.observed-element');
+
+// Intersection Observer 생성 및 대상 요소들에 적용
+observedElements.forEach(observedElement => {
+  const observer = new IntersectionObserver(function(entries, observer) {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add('active');
+      } else {
+        entry.target.classList.remove('active');
+      }
+    });
+  });
+
+  // Observer 시작
+  observer.observe(observedElement);
+});
+}
 
 
 document.addEventListener('DOMContentLoaded', function () {
         //배너슬라이드
         bannerslide();
         //인물소개슬라이드
+        profileslide();
+        //섹션 쇼 이벤트
+        opserver();
 });
